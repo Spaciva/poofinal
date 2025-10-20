3ead0646a350c8d983ad2485d2527ef3c82019fc
Francisco <fa098138@gmail.com>
2025-10-20 16:28:38 -0600

    subiendo extras

--- src/main/java/udb/proyectofinalpoo/dao/AutorDao.java
+++ src/main/java/udb/proyectofinalpoo/dao/AutorDao.java
@@ -1,4 +1,11 @@
 package udb.proyectofinalpoo.dao;
+import udb.proyectofinalpoo.model.Autor;
+import java.util.List;
 
-public class AutorDao {
+public interface AutorDao {
+    List<Autor> listar();
+    Autor buscarPorId(int idAutor);
+    void insertar(Autor a);
+    void actualizar(Autor a);
+    void eliminar(int idAutor);
 }
